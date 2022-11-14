75b8150567a43ae06342dec1dc52f426a776ebd1
tomii-dev <me@tomii.dev>
2022-11-14 13:41:46 +0000

    command: add options from vector member to dpp::slashcommand member in get

--- mikubot/src/command/command.cpp
+++ mikubot/src/command/command.cpp
@@ -6,3 +6,18 @@ Command::Command(const std::string& name, const std::string& desc)
     : m_name(name)
     , m_cmd(name, desc, MikuBot::cluster().me.id)
 {}
+
+Option& Command::addOption(const std::string& name, const std::string& desc, Option::Type type)
+{
+    m_options.push_back(Option(name, desc, type));
+
+    return m_options.back();
+}
+
+const dpp::slashcommand& Command::get()
+{
+    for(const Option& opt : m_options)
+        m_cmd.add_option(opt.get());
+
+    return m_cmd;
+}
