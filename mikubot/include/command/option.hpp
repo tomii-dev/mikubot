04ab57e3e21f6949eb7c81b9717025051b5a7547
tomii-dev <me@tomii.dev>
2022-11-14 13:55:07 +0000

    option: take name, desc, type in ctor
    * m_name member and getter

--- mikubot/include/command/option.hpp
+++ mikubot/include/command/option.hpp
@@ -20,11 +20,15 @@ public:
         ATTACHMENT
     };
 
-    Option(Type type);
+    Option(const std::string& name, const std::string& desc, Type type);
 
     void addChoice(const std::string& name, const dpp::command_value& val);
-
+    
+    inline const std::string& name() const { return m_name; }
     inline const dpp::command_option& get() const { return m_opt; }
+
 private:
+    std::string m_name;
+
     dpp::command_option m_opt;
 };
