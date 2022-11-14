dea796a3051eb6629ab697171a1334c175719cb5
tomii-dev <me@tomii.dev>
2022-11-14 13:41:09 +0000

    command: change Args type from const ref to allow for construction
    * "Context" slashcommand event alias
    * take 'context' arg in Command::call
    * options vector
    * move get to cpp, more complex impl
    * addOption protected method to add options to slash command

--- mikubot/include/command/command.hpp
+++ mikubot/include/command/command.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "option.hpp"
+
 #include <string>
 #include <map>
 #include <vector>
@@ -11,13 +13,23 @@ class Command
 public:
     Command(const std::string& name, const std::string& desc);
     
-    using Args = const std::map<std::string, dpp::command_value>&;
-    virtual void call(Args args) = 0;
+    using Args = std::map<std::string, dpp::command_value>;
+    using Context = const dpp::slashcommand_t&;
+
+    virtual void call(const Args& args, Context ctx) = 0;
 
     inline const std::string& name() const { return m_name; }
+    inline const std::vector<Option>& options() const { return m_options; }
     
-    inline const dpp::slashcommand& get() const { return m_cmd; }
+    // should only be called once, by CommandRegistry
+    const dpp::slashcommand& get();
+
+protected:
+    Option& addOption(const std::string& name, const std::string& desc, Option::Type type);
+
 private:
     std::string m_name;
     dpp::slashcommand m_cmd;
+
+    std::vector<Option> m_options;
 };
