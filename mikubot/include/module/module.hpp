c8f106132ce02268414327711a6e1ba8d826d660
tomii-dev <me@tomii.dev>
2022-11-14 09:30:02 +0000

    module base class

---
+++ mikubot/include/module/module.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "command/command.hpp"
+
+#include <memory>
+#include <vector>
+#include <string>
+
+#include <dpp/appcommand.h>
+
+class Module
+{
+public: 
+    virtual void init() = 0;
+    virtual const std::string& desc() = 0;
+
+protected:
+    inline void addCommand(std::unique_ptr<Command> cmd) { m_commands.push_back(std::move(cmd)); };
+
+private:
+    std::vector<std::unique_ptr<Command>> m_commands;
+
+    friend class ModuleManager;
+};
