84adbed9f08d20da79d774473d128493596edc62
tomii-dev <me@tomii.dev>
2022-11-14 13:50:40 +0000

    commandregistry: collect args from command and pass to Command::call
    * create global command in addCommand

--- mikubot/src/command/commandregistry.cpp
+++ mikubot/src/command/commandregistry.cpp
@@ -1,18 +1,30 @@
 #include "command/commandregistry.hpp"
 
+#include "mikubot.hpp"
+
 void CommandRegistry::handleCommand(const dpp::slashcommand_t& event)
 {
     const std::string name = event.command.get_command_name();
 
     if(!m_commands.count(name))
         return;
+    
+    Command* cmd = m_commands.at(name);
+    Command::Args args;
 
-    m_commands[name](std::map<std::string, dpp::command_value>());
+    for(const Option& opt : cmd->options())
+    {
+        const dpp::command_value& val = event.get_parameter(opt.name());
+        if(val.index())
+            args.emplace(opt.name(), val);
+    }
+
+    m_commands[name]->call(args, event);
 }
 
 void CommandRegistry::addCommand(Command* cmd)
 {
-    m_commands[cmd->name()] = [cmd](Command::Args args) {
-        cmd->call(args);
-    };
+    m_commands[cmd->name()] = cmd;
+    
+    MikuBot::cluster().global_command_create(cmd->get());
 }
