64d2448f276a3ac1ecc8e8f58077bdcfd6dc0d4c
tomii-dev <me@tomii.dev>
2022-11-16 12:54:23 +0000

    help: module implementation
    * use new command macros

--- mikubot/include/module/help.hpp
+++ mikubot/include/module/help.hpp
@@ -1,12 +1,14 @@
 #include "module.hpp"
 
-class HelpModule : public Module
+class Help : public Module
 {
 public:
     void init() override;
+    void postInit() override;
+    inline const std::string desc() const override { return "get help with mikubot commands."; }
 
-    inline const std::string desc() override
-    {
-        return "";
-    }
+private:
+    std::map<std::string, dpp::embed> m_helpEmbeds;
+
+    friend class HelpCommand;
 };
--- mikubot/src/module/help.cpp
+++ mikubot/src/module/help.cpp
@@ -1,32 +1,57 @@
 #include "module/help.hpp"
 
+#include "mikubot.hpp"
 #include "command/command.hpp"
 #include "module/modulemanager.hpp"
 
-DEFINE_MODULE(HelpModule)
+DEFINE_MODULE(Help)
 
-class HelpCommand : public Command
+SETUP_COMMAND(Help, Help, "help", "get help with command usage.")
 {
-public:
-    HelpCommand() : Command("help", "get help with command usage")
+    Option& moduleOpt = addOption("module", "the specific module to get help with", Option::STRING);
+    for(const std::string& moduleName : ModuleManager::modules())
+        moduleOpt.addChoice(moduleName, moduleName);
+}
+
+COMMAND(Help)
+{
+    if(!args.count("module"))
     {
-        addOption("module", "the specific module to get help with", Option::STRING)
-            .addChoice("voice", "VoiceModule");
+        for(const auto& kv : m_module->m_helpEmbeds)
+            miku().message_create(dpp::message(ctx.command.channel_id, kv.second));
+
+        return;
     }
 
-    void call(const Args& args, Context ctx) override 
+    const std::string& module = std::get<std::string>(args.at("module"));
+    ctx.reply(dpp::message(ctx.command.channel_id, m_module->m_helpEmbeds.at(module)));
+}
+
+void Help::init()
+{
+    addCommand(std::make_unique<HelpCommand>(this));
+}
+
+void Help::postInit()
+{
+    // build help embeds for modules
+    for(const std::string& moduleName : ModuleManager::modules())
     {
-        if(!args.count("module"))
+        const Module* module = ModuleManager::getModule(moduleName);
+
+        dpp::embed embed = dpp::embed()
+            .set_title(moduleName)
+            .set_description(module->desc())
+            .set_color(0x86cecb);
+       
+        for(const Command* cmd : module->commands())
         {
-            ctx.reply("test lol");
-            return;
+            std::string desc = cmd->desc() + "\n**options:**";
+            for(const Option& opt : cmd->options())
+                desc += "\n" + opt.name() + " - " + opt.desc() + " *(" + opt.typeStr() + ")*";
+            
+            embed.add_field("/" + cmd->name(), desc + '\n');
         }
-
-        ctx.reply("you provided module: " + std::get<std::string>(args.at("module")));
+        m_helpEmbeds.emplace(moduleName, embed);
     }
-};
-
-void HelpModule::init()
-{
-   addCommand(std::make_unique<HelpCommand>());
 }
