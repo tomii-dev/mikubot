081a5e46511be140619228b6ddbacd5e87b3b70b
tomii-dev <me@tomii.dev>
2022-12-17 23:25:06 +0000

    resource: fix RESOURCE macro
    * FIELD macro to check request has field in header and create local variable with its value

--- mikuserver/include/resource.hpp
+++ mikuserver/include/resource.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "response.hpp"
+
 #include <string>
 #include <memory>
 
@@ -11,9 +13,11 @@ public:
     virtual const std::string endpoint() const = 0;
 };
 
-using namespace httpserver;
+#define RESOURCE(ep, system, name, type) class name##Resource : public IResource { public: name##Resource (system* _system) : m_system(_system){} \
+    const std::string endpoint() const override { return ep; } std::shared_ptr<httpserver::http_response> render_ ##type(const httpserver::http_request&) override; \
+    static std::unique_ptr<IResource> make(system* _system) { return std::make_unique<name##Resource>(_system); } private: system* m_system;}; \
+    std::shared_ptr<httpserver::http_response> name##Resource::render_ ##type(const httpserver::http_request& req)
 
-#define RESOURCE(ep, system, name, type) class name : public IResource { public: name (system* _system) : m_system(_system){} \
-    const std::string endpoint() const override { return ep; } std::shared_ptr<http_response> render_ ##type(const http_request&) override; \
-    static std::unique_ptr<IResource> make(system* _system) { return std::make_unique<name>(_system); } private: system* m_system;}; \
-    std::shared_ptr<http_response> name::render_ ##type(const http_request& req)
+#define FIELD(field) if(!req.get_headers().count(#field)) return Response::badReq("missing '" #field "' field"); \
+    const std::string& field = req.get_header(#field);
+
