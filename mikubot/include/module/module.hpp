9c6ffe4ea1675d1e3b1f664e6faffa7ad6186019
tomii-dev <me@tomii.dev>
2022-12-17 23:12:42 +0000

    module: Status struct for method returns
    * MethodReturn struct to return status and data of given type
    * virtual destructor

--- mikubot/include/module/module.hpp
+++ mikubot/include/module/module.hpp
@@ -17,9 +17,23 @@ public:
     // helpful for modules such as Help that reference other modules
     virtual void postInit() {}
 
+    struct Status
+    {
+        enum Code { OK, NOT_OK } code;
+        const std::string msg;
+    };
+
+    template <typename ReturnType>
+    struct MethodReturn : public Status
+    {
+        ReturnType data;
+    };
+
     virtual const std::string desc() const = 0;
 
     const std::vector<const Command*> commands() const;
+
+    virtual ~Module() = default;
 
 protected:
     inline void addCommand(std::unique_ptr<Command> cmd) { m_commands.push_back(std::move(cmd)); };
