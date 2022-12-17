fc4a08d8d672ec7e15263c392a23f8524a98e78f
tomii-dev <me@tomii.dev>
2022-12-17 22:57:30 +0000

    mikubot: initialise curlpp

--- mikubot/src/mikubot.cpp
+++ mikubot/src/mikubot.cpp
@@ -7,6 +7,7 @@
 #include <assert.h>
 
 #include <dpp/dpp.h>
+#include <curlpp/cURLpp.hpp>
 
 std::unique_ptr<MikuBot> MikuBot::s_instance;
 
@@ -20,7 +21,11 @@ std::unique_ptr<MikuBot> MikuBot::s_instance;
 
 MikuBot::MikuBot() : m_moduleMgr(&m_cmdReg)
 {
+    cURLpp::initialize();
+
     m_cluster = std::make_unique<dpp::cluster>(BOT_TOKEN);
+
+    m_cluster->on_log(dpp::utility::cout_logger());
 
     m_cluster->on_slashcommand([this](const dpp::slashcommand_t& e){ m_cmdReg.handleCommand(e); });
     m_cluster->on_ready([this](const dpp::ready_t&){ m_moduleMgr.setupModules(); });
